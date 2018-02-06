be40691fefeca5cdb8a0836815d67d0f86449638
Samantha Herzog <samiherzog@samanthas-air.attlocal.net>
2018-02-06 12:18:02 -0500

    hi

--- oauth-twitter.py
+++ oauth-twitter.py
@@ -1,9 +1,13 @@
 
 from requests_oauthlib import OAuth1Session
 import secrets
+import json
 
 client_key = secrets.api_key
 client_secret = secrets.api_secret
+
+resource_owner_key = secrets.access_token
+resource_owner_secret = secrets.access_token_secret
 
 # STEP 1: GET A REQUEST TOKEN
 # We have to start by obtaining a 'request' token
@@ -14,11 +18,19 @@ client_secret = secrets.api_secret
 # so that we have the privelege to do more stuff, like request
 # authorization for a particular user.
 request_token_url = 'https://api.twitter.com/oauth/request_token'
+# protected_url = 'https://api.twitter.com/1.1/account/settings.json'
 
 oauth = OAuth1Session(client_key, client_secret=client_secret)
+# resource_owner_key=resource_owner_key, resource_owner_secret=resource_owner_secret)
 fetch_response = oauth.fetch_request_token(request_token_url)
 resource_owner_key = fetch_response.get('oauth_token')
 resource_owner_secret = fetch_response.get('oauth_token_secret')
+
+
+# protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
+# params = {'q':'food'}
+# r = oauth.get(protected_url, params=params)
+# print (r.text)
 
 
 # STEP 2: GET AUTHORIZATION FROM THE USER
@@ -90,3 +102,7 @@ protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
 params = {'q':'food'}
 r = oauth.get(protected_url, params=params)
 print (r.text)
+# print(type(r.text))
+# print(type(hello))
+# print(hello)
+# print(r.text['screen_name'])
